4a11df47de099adba2f46b70b3e0a84144cb0f02
ARY alarabia <earnfromx@gmail.com>
2026-03-30 13:09:21 +0500

    Update dramas.js

--- data/dramas.js
+++ data/dramas.js
@@ -4,14 +4,31 @@ const dramas = [
     title: "Rang De Episode 1",
     image: "https://lh3.googleusercontent.com/d/1WBnc67IjOGjiX7xinW6IQSZXGl7Y4y_U",
     video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    description: "مسلسل باكستاني مترجم"
+    description: "مسلسل باكستاني مترجم",
+
+    subtitle: `1
+00:00:01,000 --> 00:00:04,000
+مرحبا بك في الحلقة
+
+2
+00:00:05,000 --> 00:00:08,000
+مشاهدة ممتعة`
   },
+
   {
     slug: "jaan-e-jahan-ep-1",
     title: "Jaan e Jahan Episode 1",
     image: "https://via.placeholder.com/300",
     video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    description: "دراما باكستانية"
+    description: "دراما باكستانية",
+
+    subtitle: `1
+00:00:01,000 --> 00:00:03,000
+أهلا بك
+
+2
+00:00:04,000 --> 00:00:07,000
+حلقة رائعة`
   }
 ];
 
